8ffbe328d36461ba5b8f8df8df0c021ba317a7fa
Sasha9805 <pimenovs33@gmail.com>
2021-04-26 20:07:08 +0300

    Добавлены инструменты сборки

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,41 @@
-const { src, dest } = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
 
-function cssTransfer() {
+function minifyCSS() {
   return src('src/css/*.css')
-  .pipe(dest('dist/css'));
+    .pipe(cleanCSS({compatibility: 'ie7'}))
+    .pipe(dest('dist/css'));
 }
 
-exports.cssTransfer = cssTransfer;
+function transferFonts() {
+  return src('src/fonts/**/*')
+    .pipe(dest('dist/fonts'));
+}
+
+function transferJS() {
+  return src('src/js/main.js')
+    .pipe(dest('dist/js/'));
+}
+
+function transferPHP() {
+  return src('server.php')
+    .pipe(dest('dist/'));
+}
+
+function minifyHTML() {
+  return src('src/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(dest('dist/'));
+}
+
+function compressImages() {
+  return src('src/img/**/*.{png,jpg,jpeg}')
+    .pipe(tinypng({
+      key: 'V6lb5vfCDdh7L8097LDtqv4cvP5Y2XJw'
+    }))
+    .pipe(dest('dist/img/'));
+}
+
+exports.build = parallel(minifyHTML, minifyCSS, compressImages, transferFonts, transferJS, transferPHP);
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,19 @@ const path = require('path');
 
 module.exports = {
 
-  mode: 'development',
+  mode: 'production',
 
   entry: './src/js/script.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
 
   output: {
     filename: 'main.js',
